99fc53da95282793e5ebad4a3234d6a7275cf9a2
Aryannpatell <aryanp9924@gmail.com>
2024-03-15 22:09:05 +0530

    update page

--- id-nexus/app/page.jsx
+++ id-nexus/app/page.jsx
@@ -1,59 +1,53 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import Settings from "./components/settings";
-import { useRouter } from 'next/navigation';
-import { Button } from "@/components/ui/button"
-
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
   const router = useRouter();
   const handleButtonClickSignin = () => {
     // Redirect to the settings page
-    router.push('/components/signup');
+    router.push("/components/signup");
   };
   const handleButtonClickLogin = () => {
     // Redirect to the settings page
-    router.push('/components/signup');
+    router.push("/components/signup");
   };
 
   return (
-    <div className="content">      
-      <div className="card flex p-6 relative">
-        <div className="top-right-title absolute w-full top-0 right-0">
-            <h2>ID</h2>
-            <h3>self-sovereign</h3>
-        </div>
-
-        <div className="img-box flex flex-col items-center relative w-1/2">
-          <img src="login.png" alt="LOGIN" className="w-4/6" />
-          <h1>ID Nexus: Own Your Identity</h1>
+    <div className="flex w-screen h-screen">
+      <div className="bg-[rgb(49,49,49)] justify-self-start w-1/2">
+        <div className="flex flex-col justify-center items-center h-screen">
+          <img src="login.png" alt="LOGIN" className="w-4/6" />{" "}
+          <h1 className="text-3xl font-bold">ID Nexus: Own Your</h1>
+          <h1 className="text-3xl font-bold">Identity</h1>
           <h3>Control your personal data securely and privately</h3>
         </div>
-        
-        <div className="form-content relative w-1/2">
-          <div className="form flex flex-col justify-center align-middle items-center h-full w-full">
-            <h2>Log In</h2>
-            <form className="w-full">
-              <h3>Username</h3>
-              <input type="text" placeholder="Username" required />
-              <h3>Password</h3>
-              <input type="password" placeholder="Password" required />
-              <Button onClick={handleButtonClickLogin}>Login</Button>
-            </form>
-            <div className="bottom-button">
-              <div className="forgot_password">
-                <button variant="secondary">Forget Password</button>
-              </div>
-              <div className="signin-button">
-                <Button variant="secondary" onClick={handleButtonClickSignin}>Sign Up</Button>
-              </div>
-            </div>
-          </div>
+      </div>
+      <div className="bg-[rgb(22,22,22)]justify-self-end w-1/2">
+        <div className="flex flex-col gap-4 h-screen justify-center items-center">
+          <h2 className="text-4xl self-start ml-44">Log In</h2>
+          <h3>Username</h3>
+          <input
+            type="text"
+            placeholder="Username"
+            required
+            className="max-w-96"
+          />
+          <h3>Password</h3>
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            className="max-w-96"
+          />
+          <Button onClick={handleButtonClickLogin}>Login</Button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Home;
