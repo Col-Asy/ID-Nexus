ea0855e319c3a6bf18f2c82f2a7a8d06e1f9bf53
Aryannpatell <aryanp9924@gmail.com>
2024-03-16 15:02:59 +0530

    Update page.jsx

--- id-nexus/app/components/dashboard/page.jsx
+++ id-nexus/app/components/dashboard/page.jsx
@@ -9,16 +9,8 @@ const Dashboard = () => {
 
     return (
         <div className="flex w-screen h-screen">
-          <div className="bg-[rgb(49,49,49)] justify-self-start w-1/2">
-            <div className="flex flex-col justify-center items-center h-screen">
-              <img src="../dashboard.png" alt="USER" className="w-4/6" />{" "}
-              <h1 className="text-3xl font-bold">ID Nexus: Own Your</h1>
-              <h1 className="text-3xl font-bold">Identity</h1>
-              <h3>Control your personal data securely and privately</h3>
-            </div>
-          </div>
-          <div className="bg-[rgb(22,22,22)]justify-self-end w-1/2">
-            <div className="flex flex-col gap-4 h-screen justify-center items-center">
+          <div className="bg-[rgb(22,22,22)]justify-self-end w-2/3">
+            <div className="flex flex-col gap-12 h-screen justify-center items-center">
               <h2 className="text-4xl self-start ml-44">Details</h2>
               <div className="flex">
                 <h3>Username</h3>
@@ -45,6 +37,14 @@ const Dashboard = () => {
                 <p className="ps-2">Pan</p>
               </div>
               <Button className="min-w-96" variant="secondary">Change</Button>
+            </div>
+          </div>
+          <div className="bg-[rgb(49,49,49)] justify-self-start w-1/3">
+            <div className="flex flex-col justify-center items-center h-screen">
+              <img src="../dashboard.png" alt="USER" className="w-4/6" />{" "}
+              <h1 className="text-3xl font-bold">ID Nexus: Own Your</h1>
+              <h1 className="text-3xl font-bold">Identity</h1>
+              <h3>Control your personal data securely and privately</h3>
             </div>
           </div>
         </div>
