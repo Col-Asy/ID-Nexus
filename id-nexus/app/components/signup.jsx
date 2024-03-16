85e96059b5614fee984ea6a0b1d6788f6617df39
Aryannpatell <aryanp9924@gmail.com>
2024-03-16 11:53:38 +0530

    signUpPage

--- id-nexus/app/components/signup.jsx
+++ id-nexus/app/components/signup.jsx
@@ -15,16 +15,16 @@ const Signup = () => {
 
   return (
     <div className="flex w-screen h-screen">
-      <div className="bg-[rgb(49,49,49)] justify-self-start w-1/2">
+      <div className="bg-[rgb(49,49,49)] justify-self-start w-1/4">
         <div className="flex flex-col justify-center items-center h-screen">
-          <img src="login.png" alt="LOGIN" className="w-4/6" />{" "}
+          <img src="signUpImage.webp" alt="SIGNUP" className="w-4/6" />{" "}
           <h1 className="text-3xl font-bold">ID Nexus: Own Your</h1>
           <h1 className="text-3xl font-bold">Identity</h1>
           <h3>Control your personal data securely and privately</h3>
         </div>
       </div>
-      <div className="bg-[rgb(22,22,22)]justify-self-end w-1/2">
-        <div className="flex flex-col gap-4 h-screen justify-center items-center">
+      <div className="bg-[rgb(22,22,22)]justify-self-end w-3/4">
+        <div className="flex flex-col gap-0 h-screen justify-center items-center">
           <h2 className="text-4xl self-start ml-44">Sign Up</h2>
           <h3>Full Name</h3>
           <input
@@ -42,14 +42,14 @@ const Signup = () => {
           />
           <h3>Phone Number</h3>
           <input
-            type="number"
+            type="text"
             placeholder="Phone Number"
             required
             className="max-w-96"
           />
           <h3>Aadhar Number</h3>
           <input
-            type="number"
+            type="text"
             placeholder="Aadhar Number"
             required
             className="max-w-96"
