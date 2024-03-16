47061e251d838f83a7d15c5c9b46ea930af4e409
Aryannpatell <aryanp9924@gmail.com>
2024-03-16 14:02:05 +0530

    Update page.jsx

--- id-nexus/app/components/signup/page.jsx
+++ id-nexus/app/components/signup/page.jsx
@@ -14,28 +14,13 @@ const Signup = () => {
 
   return (
     <div className="flex w-screen h-screen">
-      <div className="bg-[rgb(49,49,49)] justify-self-start w-1/4">
-        <div className="flex flex-col justify-center items-center h-screen">
-          <img src="signUpImage.webp" alt="SIGNUP" className="w-4/6" />{" "}
-          <h1 className="text-3xl font-bold">ID Nexus: Own Your</h1>
-          <h1 className="text-3xl font-bold">Identity</h1>
-          <h3>Control your personal data securely and privately</h3>
-        </div>
-      </div>
-      <div className="bg-[rgb(22,22,22)]justify-self-end w-3/4">
+      <div className="bg-[rgb(22,22,22)]justify-self-end w-2/3">
         <div className="flex flex-col gap-0 h-screen justify-center items-center">
           <h2 className="text-4xl self-start ml-44">Sign Up</h2>
           <h3>Full Name</h3>
           <input
             type="text"
             placeholder="Full Name"
-            required
-            className="max-w-96"
-          />
-          <h3>Address</h3>
-          <input
-            type="text"
-            placeholder="Address"
             required
             className="max-w-96"
           />
@@ -60,10 +45,10 @@ const Signup = () => {
             required
             className="max-w-96"
           />
-          <h3>Username</h3>
+          <h3>Address</h3>
           <input
             type="text"
-            placeholder="Username"
+            placeholder="Address"
             required
             className="max-w-96"
           />
@@ -82,6 +67,14 @@ const Signup = () => {
             className="max-w-96"
           />
           <Button onClick={handleButtonClickSignup}>Sign Up</Button>
+        </div>
+      </div>
+      <div className="bg-[rgb(49,49,49)] justify-self-start w-1/3">
+        <div className="flex flex-col justify-center items-center h-screen">
+          <img src="../signUpImage.webp" alt="SIGNUP" className="w-4/6" />{" "}
+          <h1 className="text-3xl font-bold">ID Nexus: Own Your</h1>
+          <h1 className="text-3xl font-bold">Identity</h1>
+          <h5>Control your personal data securely and privately</h5>
         </div>
       </div>
     </div>
